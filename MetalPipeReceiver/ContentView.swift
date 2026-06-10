bc6be41d7e95e5701cea75a488e8d9110e5744ec
slingvector <iitr.anuj.wrk@gmail.com>
2026-06-10 17:16:14 +0530

    Initial Commit

---
+++ MetalPipeReceiver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalPipeReceiver
+//
+//  Created by Anuj Kumar on 10/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalPipeReceiver/MetalPipeReceiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalPipeReceiverApp.swift
+//  MetalPipeReceiver
+//
+//  Created by Anuj Kumar on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MetalPipeReceiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
